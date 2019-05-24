d0dd75ecf290cb56a3b470dd160d211a00c36619
Rafael Elias <rafaeleliasf@gmail.com>
2019-05-24 00:12:07 -0300

    Test new name

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ def home_page():
 def profile(name):
   #new_name = str(name) + " like to eats mangos"
   #teste new name
-	return render_template('index.html', name=name)
+  new_name = str(name)
+	return render_template('index.html', name=new_name)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
